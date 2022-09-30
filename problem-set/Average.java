f658a60689d98f4b6a2d969ad12bd3c19ae89ff3
bmana21 <bmana21@freeuni.edu.ge>
2022-09-30 11:21:38 +0400

    30 sep 11:21

---
+++ problem-set/Average.java
@@ -0,0 +1,24 @@
+/*
+ * File: FindRange.java
+ * --------------------
+ * This program is a stub for the FindRange problem, which finds the
+ * smallest and largest values in a list of integers.
+ */
+
+import acm.program.*;
+
+public class ConsoleProgrammSample extends ConsoleProgram {
+	private double getAvarage(int firstNumber, int secondNumber) {
+		int sum = firstNumber + secondNumber; // Sum of numbers
+		double Avarage = (double) sum / 2; // Cast to double and divide by 2
+		return Avarage;
+	}
+
+	public void run() {
+		int firstNumber = readInt("Enter first number: ");
+		int secondNumber = readInt("Enter second number: ");
+		println(getAvarage(firstNumber, secondNumber));
+	}
+
+}
+
