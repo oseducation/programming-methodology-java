41c18223cb4b2ba923c811504b3755a841a487d9
AkakiGurgenidze <agurg21@freeuni.edu.ge>
2022-10-01 22:15:28 +0400

    Add Solution For The Problem 'ForN'

---
+++ problem-set/ForN.java
@@ -0,0 +1,23 @@
+/*
+ * File: FindRange.java
+ * --------------------
+ * This program is a stub for the FindRange problem, which finds the
+ * smallest and largest values in a list of integers.
+ */
+
+import acm.program.*;
+
+public class ConsoleProgrammSample extends ConsoleProgram {
+
+	public void run() {
+		
+		// Read Input
+		int n = readInt();
+		
+		
+		for(int i=0;i<n;i++) {
+			println("Hello "+i);
+		}
+	}
+
+}
