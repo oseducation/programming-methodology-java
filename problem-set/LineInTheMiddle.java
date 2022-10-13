b50cc13955af267395f29ea50b60cec578b95afc
aduashvilianna <aduashvili2002ana@gmail.com>
2022-10-13 17:23:43 +0400

    Implemented solution and explanation for problem LineInTheMiddle

--- problem-set/LineInTheMiddle.java
+++ problem-set/LineInTheMiddle.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class LineInTheMiddle {
+import acm.graphics.GLine;
+import acm.program.GraphicsProgram;
+
+public class LineInTheMiddle extends GraphicsProgram {
+    public void run() {
+        drawLine();
+    }
+
+    private void drawLine() {
+        double width =  getWidth();     // get canvas width
+        double height = getHeight();    // get canvas height
+
+        GLine line = new GLine(0, height / 2, width, height / 2);
+        add(line); // add line to canvas
+    }
 }
