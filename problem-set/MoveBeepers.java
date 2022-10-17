6fbba904598dcce7ee644545911146bf9a6ee488
bmana21 <bmana21@freeuni.edu.ge>
2022-10-18 01:34:51 +0400

    Implemented solution and explanation

---
+++ problem-set/MoveBeepers.java
@@ -0,0 +1,42 @@
+/*
+ * File: MoveBeepers.java
+ * ----------------------------
+ * MoveBeepers
+ * კონსოლიდან წაიკითხეთ ორი რიცხვი, დაბეჭდეთ მათი საშუალო არითმეტიკული.
+ * დეტალური ახსნა შეგიძლიათ იხილოთ MoveBeepers.md ფაილში.
+ */
+
+import stanford.karel.*;
+
+public class KarelProgramSample extends Karel {
+	private void moveFourTimes() {
+		// Karel stands on the cell 1x1.
+		for (int i = 1; i <= 4; i++) {
+			move();
+		}
+		// Karel stands on the cell 5x1.
+	}
+
+	private void pickTenBeepers() {
+		for (int i = 1; i <= 10; i++) {
+			pickBeeper();
+		}
+		// Karel picks 10 beepers, still stands on the cell 5x1.
+	}
+
+	private void putTenBeepers() {
+		for (int i = 1; i <= 10; i++) {
+			putBeeper();
+		}
+		// Karel puts 10 beepers on the cell 6x1.
+	}
+
+	public void run() {
+		moveFourTimes();
+		pickTenBeepers();
+		move(); // Karel moves on the cell 6x1.
+		putTenBeepers();
+	}
+
+}
+
