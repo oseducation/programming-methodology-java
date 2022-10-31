703d9076490fe2ffa719c9e6e6bb2614d820030b
tamunaa <91693362+tamunaa@users.noreply.github.com>
2022-10-31 20:40:30 +0400

    ზიგ-ზაგით ვავსებ სამყაროს

--- problem-set/FillWorld.java
+++ problem-set/FillWorld.java
@@ -5,45 +5,51 @@
  * სამყარო ცარიელი არ არის და გარკვეულ(ჩვენთვის უცნობ) უჯრებში თითო(მხოლოდ ერთი)
  * ბრილიანტი დევს. ამასთან გაითვალისწინეთ, რომ სამყაროს ზომები თქვენთვის უცნობია და
  * თქვენი პროგრამა უნდა მუშაობდეს ნებისმიერი ზომის სამყაროსათვის.
- * */
+ */
 
 import stanford.karel.*;
 
 public class FillWorld extends SuperKarel {
-    public  void  run(){
-        while(frontIsClear()){
-            fillOneRow();
-            goBack();
-            goUp();
-        }
+    public void run() {
+        fillWorld();
     }
-    // გადავა შემდეგ როუზე
-    private void goUp(){
-        turnLeft();
-        if(frontIsClear()){
-            move();
-            turnRight();
+
+    void fillWorld() {
+        if (frontIsBlocked()) {
+            turnLeft();
+            fillOneLine();
+        } else {
+            while (frontIsClear()) {
+                fillOneLine();
+                goUp();
+            }
         }
     }
 
-    // დაბრუნდება ბოლოში
-    private void goBack(){
-        turnAround();
-        while(frontIsClear())move();
-        turnAround();
+    // გადავა შემდეგ როუზე
+    private void goUp() {
+        if (facingEast()) turnLeft(); //თუ მარცხნიდან მარჯვნივ ვმოძრაობდით
+        else turnRight(); // თუ მარჯვნიდან მარცხნივ მოვედით
+
+        if (frontIsClear()) {
+            move();
+            if (leftIsBlocked()) turnRight(); //მარცხენა კედლის შემთხვევაში ვუხვევთ მარჯვნივ
+            else turnLeft(); // მარჯვენას შემთხვევაში მარცხნივ
+        }
     }
+
 
     // მთლიან როუს შეავსებს თითო ბრილიანტით
-    private void fillOneRow(){
-        putBeeperSafely();
-        while(frontIsClear()){
+    private void fillOneLine() {
+        putBeeperSafely(); // of by one ბაგისთვის
+        while (frontIsClear()) {
             move();
             putBeeperSafely();
         }
     }
 
     //თუ უჯრაზე ბრილიანტი არ დევს, დადებს ბრილიანტს
-    private void putBeeperSafely(){
-        if(noBeepersPresent())putBeeper();
+    private void putBeeperSafely() {
+        if (noBeepersPresent()) putBeeper();
     }
 }
