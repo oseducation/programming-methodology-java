5a9a28d224df25d40c5c9162ee6fd22ea8a9a1c3
nkovz <nkovz21@freeuni.edu.ge>
2023-01-26 16:14:41 +0400

    add lecture-8

---
+++ lectures/8-steeple-chase/9x10.w
@@ -0,0 +1,32 @@
+Dimension: (9, 10)
+Wall: (3, 1) west
+Wall: (3, 2) west
+Wall: (3, 3) west
+Wall: (4, 1) west
+Wall: (4, 2) west
+Wall: (6, 1) west
+Wall: (6, 2) west
+Wall: (6, 3) west
+Wall: (6, 4) west
+Wall: (6, 5) west
+Wall: (6, 6) west
+Wall: (7, 1) west
+Wall: (7, 2) west
+Wall: (7, 3) west
+Wall: (7, 4) west
+Wall: (7, 5) west
+Wall: (7, 6) west
+Wall: (7, 7) west
+Wall: (9, 1) west
+Wall: (9, 2) west
+Wall: (9, 3) west
+Wall: (9, 4) west
+Wall: (9, 5) west
+Wall: (9, 6) west
+Wall: (9, 7) west
+Wall: (9, 8) west
+Wall: (9, 9) west
+Karel: (1, 1) east
+
+BeeperBag: INFINITE
+Speed: 0.00
---
+++ lectures/8-steeple-chase/SteepleChase.java
@@ -0,0 +1,42 @@
+import stanford.karel.*;
+
+public class SteepleChase extends SuperKarel {
+	
+	public void run() {
+		for (int i = 0; i < 8; i++) {
+			if (frontIsClear()) {
+				move();
+			} else {
+				jumpHurdle();
+			}
+		}
+	}
+
+	private void jumpHurdle() {
+		ascendHurdle();
+		move();
+		descendHurdle();
+	}
+
+	private void ascendHurdle() {
+		turnLeft();
+		while (rightIsBlocked()) {
+			move();
+		}
+		turnRight();
+	}
+
+	private void descendHurdle() {
+		turnRight();
+		moveToWall();
+		turnLeft();
+	}
+
+	private void moveToWall() {
+		while (frontIsClear()) {
+			move();
+		}
+	}
+
+}
+
